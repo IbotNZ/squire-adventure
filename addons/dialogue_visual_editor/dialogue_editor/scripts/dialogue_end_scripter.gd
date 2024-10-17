10cc6b1d4a6b9d499c5c557ed753f1b5f358505a
KiwiBot <83791394+IbotNZ@users.noreply.github.com>
2024-10-17 17:04:16 +1300

    Dialogue Script Update
    Added popup button. Save as backup for testing.

--- addons/dialogue_visual_editor/dialogue_editor/scripts/dialogue_end_scripter.gd
+++ addons/dialogue_visual_editor/dialogue_editor/scripts/dialogue_end_scripter.gd
@@ -3,6 +3,9 @@ extends EditorNode
 
 # the dialogue that will be set visible when dialogue end runs
 # var new_visible_dialogues
+@onready @export var button_picker_dialog := $ButtonPickerDialog
+@onready @export var button_list := $ButtonPickerDialog/ItemList
+var button_list_references: Array
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,3 +15,14 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_new_button_setter_pressed() -> void:
+	button_list.clear()
+	button_list_references.clear()
+	for i in EditorInterface.get_edited_scene_root().get_children():
+		if i is Area3D:
+			for dialogue in i.active_choices:
+				button_list.add_item(dialogue.name)
+				button_list_references.append(dialogue)
+	button_picker_dialog.popup()

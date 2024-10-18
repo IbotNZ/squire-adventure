b9e952c9fc6d05c94d031f9563ef3db5dad7465e
KiwiBot <83791394+IbotNZ@users.noreply.github.com>
2024-10-18 14:47:21 +1300

    Button Added To End Scripter

--- addons/dialogue_visual_editor/dialogue_editor/scripts/dialogue_end_scripter.gd
+++ addons/dialogue_visual_editor/dialogue_editor/scripts/dialogue_end_scripter.gd
@@ -6,6 +6,7 @@ extends EditorNode
 @onready @export var assign_button := $NewButtonSetter
 @onready @export var button_picker_dialog := $ButtonPickerDialog
 @onready @export var button_list := $ButtonPickerDialog/ItemList
+@onready @export var button_selectable_setter := $IsButtonSelectableSetter
 var button_list_references: Array
 
 # Called when the node enters the scene tree for the first time.
@@ -21,6 +22,7 @@ func _process(delta: float) -> void:
 func sync_with_node():
 	if linked_node != null:
 		assign_button.text = linked_node.new_visible_dialogue.button_title
+		button_selectable_setter.toggle_mode = linked_node.is_dialogue_available
 
 
 func _on_new_button_setter_pressed() -> void:
@@ -42,3 +44,7 @@ func _on_item_list_item_clicked(index: int, at_position: Vector2, mouse_button_i
 
 func _on_position_offset_changed() -> void:
 	linked_node.graph_position = position_offset
+
+
+func _on_is_button_selectable_setter_toggled(toggled_on: bool) -> void:
+	linked_node.is_dialogue_available = toggled_on
--- addons/dialogue_visual_editor/dialogue_objects/dialogue_end_scripter.gd
+++ addons/dialogue_visual_editor/dialogue_objects/dialogue_end_scripter.gd
@@ -3,3 +3,4 @@ class_name DialogueEndScripter
 
 @export var new_visible_dialogue: DialogueNode
 @export var next_dialogue_node: DialogueType
+@export var is_dialogue_available: bool = true

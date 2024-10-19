cc48d41caf3580ad1b2e980b171b45da6ce3be80
KiwiBot <83791394+IbotNZ@users.noreply.github.com>
2024-10-19 17:24:40 +1300

    Completely Cleared File Structure

--- addons/dialogue_visual_editor/dialogue_objects/dialogue_type.gd
+++
@@ -1,6 +0,0 @@
-extends Resource
-class_name DialogueType
-
-var graph_position_x: float
-var graph_position_y: float
-@export var graph_position: Vector2 = Vector2(0,0)
--- addons/dialogue_visual_editor/dialogue_objects/dialogue_variable.gd
+++
@@ -1,7 +0,0 @@
-extends DialogueType
-class_name DialogueVariable
-
-@export var var_name: String
-@export var default_bool: bool
-@export var default_number: float
-@export var node_type: int
